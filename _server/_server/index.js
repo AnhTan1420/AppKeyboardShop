eb126cc95151aa8da88547f48ef1791379132086
minhchiu <minhch.vn@gmail.com>
2021-12-24 13:12:47 +0700

    [minhch] Finish chatbot in server

--- _server/_server/index.js
+++ _server/_server/index.js
@@ -9,7 +9,25 @@ app.get('/', (req, res) => {
 const server = app.listen(port, () => {
 	console.log(`Example app listening at http://localhost:${port}`)
 })
+
 const io = require('socket.io')(server)
+
+const connectedUser = new Set()
 io.on('connection', socket => {
+	// INit
 	console.log('Connected Successfully', socket.id)
+	connectedUser.add(socket.id)
+	io.emit('connected-user', connectedUser.size)
+
+	// Disconnect
+	socket.on('disconnect', () => {
+		console.log('Disconnected ', socket.id)
+		connectedUser.delete(socket.id)
+	})
+
+	// Message
+	socket.on('message', data => {
+		console.log(data)
+		socket.broadcast.emit('message-receive', data)
+	})
 })
